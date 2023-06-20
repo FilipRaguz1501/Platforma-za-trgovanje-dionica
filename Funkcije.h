58170e79d4a7343c1f0f692333101022cb3d590c
FilipRaguz1501 <126962059+FilipRaguz1501@users.noreply.github.com>
2023-06-21 00:48:26 +0200

    Update Funkcije.h

--- Funkcije.h
+++ Funkcije.h
@@ -1,48 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #define _CRT_SECURE_NO_WARNINGS
 #ifndef FUNKCIJE_H
 #define FUNKCIJE_H
-#define MAX_IME 50
-#define MAX_LOZINKA 20
-#include <stdio.h>
+#define ISPISI_BUDZET(budzet) printf("\nVaÅ¡ budÅ¾et iznosi: %.2f EUR.\n", budzet);
+typedef struct Portfelj Portfelj;  // Naprijed deklaracija strukture Portfelj
 
 typedef struct {
-	char naziv[10]; //2. Odabir konkretnih složenih tipova podataka za rad sa specifiènim objektima.
-	float cijena;
-	int brojJedinica;  // 1. Odabir konkretnih primitivnih tipova podataka za rad s cjelobrojnim i realnim konstantama.
-} Dionica;
-
-typedef struct {
-	Dionica** dionice;  // Pokazivaè na pokazivaèe na Dionica
-	int brojDionica;   // Trenutni broj dionica u portfelju
-} Portfelj;
-
-typedef struct { //3. Primjena typedef sa strukturama i unijama, po potrebi s enum tipovima podataka tamo gdje
-	char ime[MAX_IME]; // 4. Imenovanje identifikatora (varijabli, konstanti, polja, funkcija, pokazivaèa…) 
-	char lozinka[MAX_LOZINKA];
-	Portfelj portfelj;
-	double budzet;
+    char korisnickoIme[50];
+    char lozinka[50];
+    double budzet;
+    Portfelj* portfelj;    // Dodavanje portfelj u strukturu Korisnik
 } Korisnik;
 
-Dionica* novaDionica(const char* naziv, float cijena);
+typedef struct {
+    char naziv[50];
+    double cijena;
+    int kolicina;
+} Dionica;
 
-// Implementacija kao inline funkcija
-static inline void ispisDionice(const Dionica* dionica) { //7.Ako su funkcije jednostavne koristiti makro funkcije ili inline funkcije
-	if (dionica == NULL) {
-		fprintf(stderr, "Pogreška: Dionica ne može biti NULL\n");
-		return;
-	}
-	printf("Naziv dionice: %s, Cijena: %.2f\n", dionica->naziv, dionica->cijena);
-}
+struct Portfelj {
+    Dionica* dionice;
+    int brojDionica;
+    int kapacitet;
+};
 
-void oslobodiPortfelj(Portfelj* portfelj);
-void oslobodiDionicu(Dionica* dionica);
-int ukloni_dionicu(Portfelj* portfelj, char* naziv);
-int dodaj_dionicu(Portfelj* portfelj, Dionica* novaDionica);
-void izracunaj_vrijednost_portfelja(Portfelj* portfelj);
-Korisnik* kreirajKorisnika(char* ime, char* lozinka, double polog);
-void dodajKorisnikaUDatoteku(Korisnik* korisnik);
-int provjeriKorisnika(const char* ime, const char* lozinka);
-int dodajNovacKorisniku(const char* ime, double iznos);
-Korisnik* registracija();
-Korisnik* prijava();
+Korisnik* kreirajKorisnika(const char* korisnickoIme, const char* lozinka, double budzet);
+void obrisiKorisnika(Korisnik* korisnik);
+Dionica* kreirajDionicu(const char* naziv, double cijena);
+void obrisiDionicu(Dionica* dionica);
+Portfelj* kreirajPortfelj();
+void obrisiPortfelj(Portfelj* portfelj);
+bool provjeri_bazu_podataka(FILE* datoteka, const char* korisnicko_ime, const char* lozinka);
+Korisnik* prijava(FILE* datoteka, int brojKorisnika, Korisnik* korisnici);
+//2 ->
+Korisnik* registracija(FILE* datoteka, int brojKorisnika, Korisnik* korisnici);
+bool kupiDionicu(Portfelj* portfelj, Dionica* dionica, int kolicina, double* budzet);
+void prodajDionicu(Portfelj* portfelj, Dionica* dionica, int kolicina, double* budzet);
+Dionica* provjeriDionicu(Dionica* dionice, int brojDionica);
+void depozit(Korisnik* korisnik, double iznos);
+int usporediDionicePoCijeni(const void* a, const void* b);
+void sortirajPortfeljPoCijeni(Portfelj* portfelj);
+int usporediDionicePoNazivuZaPretragu(const void* a, const void* b);
+void sortirajPortfeljPoNazivu(Portfelj* portfelj);
+Dionica* pretraziPortfelj(Portfelj* portfelj, char* nazivDionice);
+void ispisiPortfelj(Portfelj* portfelj);
+void ispisiBudzet(double budzet);
+void obrisiDatotekuKorisnika(const char* imeDatoteke);
 #endif
